b37b5cbba4d09f9971a4c39420f8f77454c48659
AbdouTheWD <gachtou3@gmail.com>
2024-08-05 20:12:33 +0100

    duplicate word

--- src/components/Frame/Frame.jsx
+++ src/components/Frame/Frame.jsx
@@ -1,47 +1,15 @@
-import React, { useEffect, useRef } from "react";
 import blackFrameLandscape from "../../assets/frames/frame black landscape.png";
 import blackFramePortrait from "../../assets/frames/frame black portrait.png";
 import whiteFrameLandscape from "../../assets/frames/frame white landscape.png";
 import whiteFramePortrait from "../../assets/frames/frame white portrait.png";
 import goldFrameLandscape from "../../assets/frames/frame gold landscape.png";
 import goldFramePortrait from "../../assets/frames/frame gold portrait.png";
-import interact from "interactjs";
-
-function Quote({ text }) {
-  const quoteRef = useRef(null);
-  const position = { x: 0, y: 0 };
-  useEffect(() => {
-    interact(quoteRef.current).draggable({
-      listeners: {
-        move(event) {
-          position.x += event.dx;
-          position.y += event.dy;
-
-          event.target.style.transform = `translate(${position.x}px, ${position.y}px)`;
-        },
-      },
-      inertia: true,
-      modifiers: [
-        interact.modifiers.restrictRect({
-          restriction: "parent",
-          endOnly: true,
-        }),
-      ],
-    });
-  }, []);
-
-  return (
-    <p
-      contentEditable
-      className="quote absolute top-20 left-1/2 -translate-x-1/2 font-semibold touch-none select-none text-lg text-center whitespace-nowrap"
-      ref={quoteRef}
-    >
-      {text}
-    </p>
-  );
-}
+import Quote from "./Quote";
+import { useState } from "react";
 
 function Frame() {
+  const [quotes, setQuotes] = useState(["و خلقناكم أزواجا"]);
+
   return (
     <section
       id="frame"
@@ -50,11 +18,15 @@ function Frame() {
     >
       <div className="w-full h-full max-md:h-fit m-auto relative">
         <img
-          src={goldFramePortrait}
+          src={blackFramePortrait}
           alt=""
           className="w-full h-full object-contain"
+          draggable="false"
         />
-        <Quote text="TEST1" />
+        {quotes.length > 0 &&
+          quotes.map((quote) => (
+            <Quote text={quote} quotes={quotes} setQuotes={setQuotes} />
+          ))}
       </div>
     </section>
   );
---
+++ src/components/Frame/Quote.jsx
@@ -0,0 +1,39 @@
+import interact from "interactjs";
+import { useEffect, useRef } from "react";
+
+function Quote({ text, quotes, setQuotes }) {
+  const quoteRef = useRef(null);
+  const position = { x: 0, y: 0 };
+  useEffect(() => {
+    interact(quoteRef.current).draggable({
+      listeners: {
+        move(event) {
+          position.x += event.dx;
+          position.y += event.dy;
+
+          event.target.style.transform = `translate(${position.x}px, ${position.y}px)`;
+        },
+      },
+      inertia: true,
+      modifiers: [
+        interact.modifiers.restrictRect({
+          restriction: "parent",
+          endOnly: true,
+        }),
+      ],
+    });
+  }, []);
+
+  return (
+    <p
+      contentEditable
+      className="quote absolute top-20 left-1/2 -translate-x-1/2 font-semibold touch-none select-none text-lg text-center whitespace-nowrap"
+      ref={quoteRef}
+      onDoubleClick={() => setQuotes([...quotes, text])}
+    >
+      {text}
+    </p>
+  );
+}
+
+export default Quote;
