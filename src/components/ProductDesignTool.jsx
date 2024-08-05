d35968c4f48050ea5234d59b8aa6266dac6370a3
AbdouTheWD <gachtou3@gmail.com>
2024-08-05 18:02:03 +0100

    add frame buttons

--- src/components/ProductDesignTool.jsx
+++ src/components/ProductDesignTool.jsx
@@ -3,8 +3,23 @@ import Frame from "./Frame/Frame";
 function ProductDesignTool() {
   return (
     <section className="product-design-tool w-screen">
-      <div className="container w-3/5 m-auto" style={{ height: "90vh" }}>
+      <div className="container w-fit m-auto" style={{ height: "90vh" }}>
         <Frame />
+        <div
+          className="actions-buttons w-full flex justify-between items-center"
+          style={{ height: "10vh" }}
+        >
+          <div className="edit-frame-button h-14" style={{ width: "48%" }}>
+            <button className="w-full h-full rounded-md bg-blue-500 hover:bg-blue-600 text-white font-semibold">
+              تعديل الاطار
+            </button>
+          </div>
+          <div className="*checkout-button h-14" style={{ width: "48%" }}>
+            <button className="w-full h-full rounded-md bg-green-500 hover:bg-green-600 text-white font-semibold">
+              تأكيد الطلبية
+            </button>
+          </div>
+        </div>
       </div>
     </section>
   );
