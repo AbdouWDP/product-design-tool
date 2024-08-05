3777be4ea825988d905e3957737996a8298b92df
AbdouTheWD <gachtou3@gmail.com>
2024-08-05 21:00:11 +0100

    add overlay and edit form

--- src/components/Frame/ActionsButtons.jsx
+++ src/components/Frame/ActionsButtons.jsx
@@ -1,11 +1,14 @@
-function ActionsButtons() {
+function ActionsButtons({ setIsEdit }) {
   return (
     <section
       className="actions-buttons w-full flex justify-between items-center"
       style={{ height: "10vh" }}
     >
       <div className="edit-frame-button h-14" style={{ width: "48%" }}>
-        <button className="w-full h-full rounded-md bg-blue-500 hover:bg-blue-600 text-white font-semibold">
+        <button
+          className="w-full h-full rounded-md bg-blue-500 hover:bg-blue-600 text-white font-semibold"
+          onClick={() => setIsEdit(true)}
+        >
           تعديل الاطار
         </button>
       </div>
--- src/components/ProductDesignTool.jsx
+++ src/components/ProductDesignTool.jsx
@@ -1,14 +1,39 @@
+import { useState } from "react";
 import ActionsButtons from "./Frame/ActionsButtons";
 import Frame from "./Frame/Frame";
 
 function ProductDesignTool() {
+  const [isEdit, setIsEdit] = useState(null);
+
   return (
-    <section className="product-design-tool w-screen max-md:m-auto">
-      <div className="container w-fit h-full m-auto">
-        <Frame />
-        <ActionsButtons />
-      </div>
-    </section>
+    <>
+      {isEdit && (
+        <>
+          <div
+            className="overlay w-screen h-screen absolute top-0 left-0 z-40"
+            style={{ background: "rgba(0,0,0,0.9)" }}
+            onClick={() => setIsEdit(null)}
+          ></div>
+          <div className="edit-frame-form w-1/2 h-1/2 bg-red-500 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 flex justify-center items-center">
+            <form
+              style={{ width: "95%", height: "95%" }}
+              onSubmit={(e) => {
+                e.preventDefault();
+                setIsEdit(null);
+              }}
+            >
+              <button className="w-40 h-10 bg-blue-500">Submit</button>
+            </form>
+          </div>
+        </>
+      )}
+      <section className="product-design-tool w-screen max-md:m-auto">
+        <div className="container w-fit h-full m-auto">
+          <Frame />
+          <ActionsButtons setIsEdit={setIsEdit} />
+        </div>
+      </section>
+    </>
   );
 }
 
