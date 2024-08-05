f25915ed4ec75e28d94253a2b3ca8ebb2c19e49e
AbdouTheWD <gachtou3@gmail.com>
2024-08-05 18:37:15 +0100

    height svh

--- src/components/ProductDesignTool.jsx
+++ src/components/ProductDesignTool.jsx
@@ -2,7 +2,7 @@ import Frame from "./Frame/Frame";
 
 function ProductDesignTool() {
   return (
-    <section className="product-design-tool w-screen h-screen max-lg:h-dvh">
+    <section className="product-design-tool w-screen h-screen max-lg:h-svh">
       <div className="container w-fit m-auto" style={{ height: "90vh" }}>
         <Frame />
         <div
